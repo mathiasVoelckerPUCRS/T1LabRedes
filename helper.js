594eb4b9cf3e416f33e071b71213c1b5ae567614
Mateus Haas <mateus@mhaas.com.br>
2018-09-15 15:28:47 -0300

    Implementação quase final do server

---
+++ helper.js
@@ -0,0 +1,42 @@
+const config = require("./config")
+
+module.exports = {
+    parseArgs: data => {
+        return data.toString()
+            .trim()
+            .split("|")
+            .map(e => e.replace("-", " "))
+    },
+
+    //Applied shorter name for these variables (will look better in code)
+    //'m' stands for -> Matrix (array of 9 elements)
+    //'c' stands for -> Character (X or O)
+    didIWin: (m, c) => {
+        //Horizontal
+        for (let h in [0, 3, 6])
+            if (m[h] === c && m[h + 1] === c && m[h + 2] === c)
+                return true
+
+        //Vertical
+        for (let v in [0, 1, 2])
+            if (m[v] === c && m[v + 3] === c && m[v + 6] === c)
+                return true
+
+        //Diagonal
+        for (let d in [[0, 8], [2, 6]])
+            if (m[4] === c && m[d[0]] === c && m[d[1]] === c)
+                return true
+
+        return false
+    },
+
+    response: {
+        success: result => {
+            return `success${config.separator}${(result || "")}`
+        },
+
+        error: result => {
+            return `error${config.separator}${(result || "")}`
+        },
+    }
+}
---
+++ server.js
@@ -0,0 +1,175 @@
+/*
+ * Nomes: Mateus Haas e Mathias Voelcker
+ * Implementação: Server TCP
+ */
+
+const helper = require("./helper")
+const config = require("./config")
+const net = require('net')
+
+//List of all games 
+var games = []
+
+//List of clients of the server
+var clients = []
+
+var server = net.createServer(client => {
+    let scope = this
+
+    clients.push(client)
+
+	client.on('error', err => {
+	   console.log("Error happened: " + err)
+	})
+
+	client.on('end', () => {
+    	console.log("Closed connection.")
+  	})
+
+	client.on('data', data => {
+        let args = helper.parseArgs(data)
+
+        //Find the respective event the client is trying to call
+        let event = "_on" + args[0] + "Event"
+
+        //Validate if the event is valid
+        if (!scope[event])
+            return client.write(`Event '${args[0]}' not found.`)
+
+        //Execute the event and store the result
+        let result = scope[event](args)
+
+        //Return the result
+        client.write(args[0] + config.separator + result)
+    })
+
+    //Creation of new game
+    scope._onNewGameEvent = (args) => {
+        //check amount of arguments
+        if (args.length < 2)
+            return helper.response.error("invalid amount of args")
+
+        //add the new game into the game's list
+        games.push({
+            name: args[1], //Game name
+            owner: client, //Game's owner
+            partner: null, //Game's partner (we don't have yet)
+            matrix: [ '', '', '', '', '', '', '', '', '' ] //Game matrix
+        })
+
+        //return to user the game id
+        return helper.response.success(games.length -1)
+    }
+
+    //List of all existing games
+    scope._onListGamesEvent = (args) => {
+        //Consolidate list of all games
+        let result = games.map((g, k) => {
+            return [
+                k, //Game key
+                g.name //Game name
+            ].join(config.separator)
+        }).join(`${config.separator};${config.separator}`)
+
+        //return the list for the client
+        return helper.response.success(result)
+    }
+
+    //Someone wants to join the game
+    scope._onJoinGameEvent = (args) => {
+        let gameKey = parseInt(args[1])
+
+        //check if the game exists
+        if (!games[gameKey])
+            return helper.response.error("invalid game key")
+
+        //Check if the game is already in place
+        if (games[gameKey].partner !== null)
+            return helper.response.error("game is already being played")
+
+        //Get the game and update the partner
+        games[gameKey].partner = client
+
+        //let the owner know we are ready to play
+        games[gameKey].client.write("ready")
+
+        return helper.response.success()
+    }
+
+    //Game summary
+    scope._onGameSummaryEvent = (args) => {
+        let gameKey = parseInt(args[1])
+        
+        //check if the game exists
+        if (!games[gameKey])
+            return helper.response.error("invalid game key")
+
+        //Get the game
+        let game = games[gameKey]
+
+        //Check if the game is yours
+        if (game.partner !== client && game.owner !== client)
+            return helper.response.error("game is not yours (duh)")
+
+        //Get the matrix
+        let result = game.matrix.split(config.separator)
+
+        return helper.response.success(result)
+    }
+
+    //Game play
+    scope._onGamePlayEvent = (args) => {
+        //check amount of arguments
+        if (args.length < 2)
+            return helper.response.error("invalid amount of args")
+
+        let key = parseInt(args[1])
+        let move = parseInt(args[2])
+
+        //check if the game exists
+        if (!games[key])
+            return helper.response.error("invalid game key")
+
+        //Get the game
+        let game = games[key]
+
+        //Check if the game is yours
+        if (game.partner !== client && game.owner !== client)
+            return helper.response.error("game is not yours (duh)")
+
+        //Get which character I am
+        let character = game.owner === client ? "X" : "O"
+
+        //check if the move is possible
+        if (move < 0 || move > 8)
+            return helper.response.error("move out of range")
+
+        //check if the position was already taken
+        if (game.matrix[move] !== '')
+            return helper.response.error("position already taken")
+
+        //commit the position
+        game.matrix[move] = character
+
+        //get the player two
+        let playerTwo = game.owner === client ? game.partner : game.owner
+
+        //check if I win
+        if (helper.didIWin(game.matrix, character)) {
+            //let the clients know that the game is done
+            client.write("YouWin")
+            playerTwo.write("YouLoose")
+
+            //TODO: must end the game
+        }
+        else
+            //let the other player know it's his turn to play  
+            playerTwo.write("YourTurn")
+
+        return helper.response.success()
+    }
+})
+
+server.listen(config.port, '127.0.0.1')
+
+console.log(`Server running on port '${config.port}'!`)
